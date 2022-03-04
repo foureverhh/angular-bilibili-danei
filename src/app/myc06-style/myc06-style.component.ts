d889d17a10dcb06777bb577b7753f182c281879a
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-03-04 11:55:38 +0100

    add bootrap

---
+++ src/app/myc06-style/myc06-style.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myc06-style',
+  templateUrl: './myc06-style.component.html',
+  styleUrls: ['./myc06-style.component.css']
+})
+export class Myc06StyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
