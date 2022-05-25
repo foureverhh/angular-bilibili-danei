1c40ab75a41957c49249ba759977f855c9bfbfd8
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-05-25 15:21:04 +0200

    child update value to parent via @Output event = new EventEmtter()

--- src/app/child01-modify/child01-modify.component.ts
+++ src/app/child01-modify/child01-modify.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child01-modify',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child01-modify.component.scss']
 })
 export class Child01ModifyComponent implements OnInit {
+  
+  userInput!:string;
+
+  @Output() //事件发射器，输出型属性，可以向父组件输出数据 @angular/core
+  buttonClickedEvent = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeUserName() {
+    console.log('userInput', this.userInput);
+    //子组件向父组件发射this.userInput数据
+    this.buttonClickedEvent.emit(this.userInput);
+  }
 }
--- src/app/parent-blog/parent-blog.component.ts
+++ src/app/parent-blog/parent-blog.component.ts
@@ -14,4 +14,9 @@ export class ParentBlogComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //处理子组件的buttonClickedEvent, e就是子组件通过@output事件发送过来的this.userInput
+  handleButtonClickedInChild01(e:any) {
+    console.log('event', e);
+    this.username = e;
+  }
 }
