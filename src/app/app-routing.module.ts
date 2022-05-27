78427ab11a80ef1c76c62873201f4c92b9c394ac
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-05-27 07:49:09 +0200

    path:'**' for all matches in router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { StartPageComponent } from './routerLeaning/start-page/start-page.compon
 import { ProductListComponent } from './routerLeaning/product-list/product-list.component';
 import { ProductDetailComponent } from './routerLeaning/product-detail/product-detail.component';
 import { UserCenterComponent } from './routerLeaning/user-center/user-center.component';
+import { NotFoundPageComponent } from './routerLeaning/not-found-page/not-found-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'start', pathMatch: 'full' },
   { path: 'start', component: StartPageComponent },
   { path: 'list', component: ProductListComponent},
   { path: 'detail', component: ProductDetailComponent },
   { path: 'center', component: UserCenterComponent },
-  { path: '', redirectTo: 'start', pathMatch: 'full' },
+  { path: '**', component: NotFoundPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { StartPageComponent } from './routerLeaning/start-page/start-page.compon
 import { ProductListComponent } from './routerLeaning/product-list/product-list.component';
 import { ProductDetailComponent } from './routerLeaning/product-detail/product-detail.component';
 import { UserCenterComponent } from './routerLeaning/user-center/user-center.component';
+import { NotFoundPageComponent } from './routerLeaning/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
@@ -66,6 +67,7 @@ import { UserCenterComponent } from './routerLeaning/user-center/user-center.com
     ProductListComponent,
     ProductDetailComponent,
     UserCenterComponent,
+    NotFoundPageComponent,
   ],
   imports: [
     BrowserModule,
